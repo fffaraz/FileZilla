2df69b9b47cf5b1886407389443c95fbe6919d60
Faraz Fallahi <fffaraz@gmail.com>
2025-09-16 02:26:11 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,3 +11,7 @@ docker rm -f filezilla
 
 # docker run --privileged -it --entrypoint /bin/bash filezilla
 # cd /opt && ./appimagetool-x86_64.AppImage ./approot ./FileZilla.AppImage
+
+# source code:
+# https://filezilla-project.org/download.php?show_all=1
+# https://lib.filezilla-project.org/download.php
