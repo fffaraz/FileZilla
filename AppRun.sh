508609346e130a516050aceef04c6ce4d6ddaa71
Faraz Fallahi <fffaraz@gmail.com>
2026-03-21 14:13:43 -0700

    update

--- AppRun.sh
+++ AppRun.sh
@@ -7,5 +7,7 @@ EXEC="${HERE}/opt/filezilla/filezilla"
 
 export LD_LIBRARY_PATH="/usr/lib:${HERE}/opt/filezilla"
 export FZ_DATADIR="${HERE}/opt/filezilla/share"
+export FZ_FZSFTP="${HERE}/opt/filezilla/fzsftp"
+export FZ_FZPUTTYGEN="${HERE}/opt/filezilla/fzputtygen"
 
 exec "${EXEC}"
