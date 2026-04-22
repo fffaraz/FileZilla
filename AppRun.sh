93b17ee86f491fc340bcb8b5d11cd5ed33e1d12b
Faraz Fallahi <fffaraz@gmail.com>
2026-04-21 22:07:09 -0700

    Update AppRun.sh

--- AppRun.sh
+++ AppRun.sh
@@ -3,11 +3,10 @@
 
 SELF=$(readlink -f "$0")
 HERE=${SELF%/*}
-EXEC="${HERE}/opt/filezilla/filezilla"
 
 export LD_LIBRARY_PATH="${HERE}/opt/filezilla:/usr/lib"
 export FZ_DATADIR="${HERE}/opt/filezilla/share"
 export FZ_FZSFTP="${HERE}/opt/filezilla/fzsftp"
 export FZ_FZPUTTYGEN="${HERE}/opt/filezilla/fzputtygen"
 
-exec "${EXEC}"
+exec "${HERE}/opt/filezilla/filezilla" "$@"
